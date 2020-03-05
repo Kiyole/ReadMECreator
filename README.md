269b35919812bb5bc682d798545cba59ae87ca51
Kiyole <Kasmith8100@gmail.com>
2020-03-04 23:10:52 -0500

    adding readMe generator, checking if node modules are being pushed

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ReadMECreator
+
+# as
+### A Quick Description ### 
+as
+
+## Table of Contents
+- Installation
+- Usage
+- License
+- Contributing
+- Tests
+- Questions
+
+## Installation
+Before we dive into the application make sure you have the following items and packages installed:  
+as
+
+## Usage  
+Some tips and general knowledge you should have about this app:
+as  
+
+## License
+This project is licensed under the MIT License located at: [MIT License](https://opensource.org/licenses/MIT)  
+
+## Contributing
+To contibute to this repository please first discuss changes you would like to make with the owner via email which will be found towards the bottom of the page.
+Our code of contact can be found at: [Code of Conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct/)
+
+## Tests
+To run the tests we have for this project follow these steps:
+as
+### What do these tests do?
+as
+
+## Questions
+[GitHub Pic](https://avatars3.githubusercontent.com/u/57542547?v=4)
+If you have any other questions or concerns i can be reached via null
--- index.js
+++ index.js
@@ -1,132 +1,146 @@
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
-const writeFileASync = util.promisify(fs.writeFile);
+const axios = require('axios');
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// async function getGit(){
-//     const user = 'Kiyole'
-//     const url = 'https://api.github.com/search/' + user;
-//     const response = await fetch(url)
-//     const result = await response.json()
-//     console.log(result)
-// }
-// getGit();
-function promptUser(){
-
-
-
-return inquirer.prompt([{
-    type: 'input',
-    message: 'What is your Name',
-    name: 'name',
-
-},
-{
-     type: 'input',
-    message: 'What is your GitHub username',
-    name: 'Git',
-},
-{
-    type: 'input',
-    message: 'Whats a basic description for the project',
-    name: 'description',
-},
-{
-   type: 'input',
-    message: 'What is the title of your project',
-    name: 'title',
-},
-{
-    type: 'input',
-    message: 'What is the purpose of your app',
-    name: 'purpose'
-
-},
-{
-    type: 'input',
-    message: 'If there are packages you need to install, list them below',
-    name: 'installation',
-},
-{
-    type: 'input',
-    message: 'What are some things you need people to know about how to use your project prior to downloading it',
-    name: 'usage'
-}
-])
+async function getGit(answers) {
+    const user = answers.Git;
+    const url = 'https://api.github.com/users/' + user;
+    const response = await axios.get(url)
+    // const result = await response.json()
+    return response;
 }
 
-function generateHTML(answers){
+
+function promptUser() {
+
+
+
+    return inquirer.prompt([{
+        type: 'input',
+        message: 'What is your Name',
+        name: 'name',
+
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username',
+        name: 'Git',
+    },
+    {
+        type: 'input',
+        message: 'Whats a basic description for the project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the purpose of your app',
+        name: 'purpose'
+
+    },
+    {
+        type: 'input',
+        message: 'If there are packages you need to install, list them below',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What are things you need people to know about how to use your project prior to downloading it',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'How do you run the tests that are used in this Project',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'Explain what the tests do and why',
+        name: 'testReason'
+    }
+    ])
+}
+
+function generateHTML(answers, data) {
     console.log("yes");
+    console.log(data)
     console.log(answers);
 
-    
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <title>ReadMe Generator</title>
-    </head>
-    
-    <body>
-        <div class="container">
-            <img class="gitProfile" src="GitHub">
-            <h2>Hi my name is ${answers.name} this is a walkthrough of my application..</h2>
-            <h1>Project Name</h1>
-            <div class="jumbotron">
-                <h2><ul>Table of Contents</ul></h2>
-                <li><a>Description</a></li>
-                <li><a>Installation</a></li>
-                <li><a>Usage</a></li>
-                <li><a>License</a></li>
-                <li><a>Contributing</a></li>
-                <li><a>Questions</a></li>
-            </div>
-            <div class="description">
-                <h2>A Quick Description:</h2>
-                <p>${answers.description}</p>
-            </div>
-            <div class="installation">
-                <h2>Installation</h2>
-                <p>Before diving into the application it is recommended you download:${answers.installation}</p>
-            </div> 
-            <div class="usage">
-                Some pointers while getting started on the app are as follows:${answers.usage}
-            </div>
-            <div class="license">
-                <h3>Standard MIT license referenced below</h3>
-                <p>Copyright(c) [2020] [${answers.name}]
-                    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-                    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-                </p>
-            </div>
-            <div class="contributing">
-                <h2>Contributing</h2>
-            </div>
-            <div class="tests">
-                <h2>Tests</h2>
-            </div>
-            <div class="Questions">
-                <h2>Questions</h2>
-            </div>
-        </div>
-    </body>
-    
-    </html>
-    `
+
+    return `
+# ${answers.title}
+### A Quick Description ### 
+${answers.description}
+
+## Table of Contents
+- Installation
+- Usage
+- License
+- Contributing
+- Tests
+- Questions
+
+## Installation
+Before we dive into the application make sure you have the following items and packages installed:  
+${answers.installation}
+
+## Usage  
+Some tips and general knowledge you should have about this app:
+${answers.usage}  
+
+## License
+This project is licensed under the MIT License located at: [MIT License](https://opensource.org/licenses/MIT)  
+
+## Contributing
+To contibute to this repository please first discuss changes you would like to make with the owner via email which will be found towards the bottom of the page.
+Our code of contact can be found at: [Code of Conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct/)
+
+## Tests
+To run the tests we have for this project follow these steps:
+${answers.test}
+### What do these tests do?
+${answers.testReason}
+
+## Questions
+[GitHub Pic](${data.avatar_url})
+If you have any other questions or concerns i can be reached via ${data.email}
+`
 }
 
-promptUser()
-.then(function (answers){
-const html = generateHTML(answers);
-console.log(html)
-return writeFileASync('index.html', html)
-}).then(function() {
-    console.log("Successfully wrote to index.html");
-  })
-.catch(function (err){
-    console.log(err)
-});
+// promptUser()
+// .then(function (answers){
+//     const html = generateHTML(answers);
+//     console.log(html)
+//     return writeFileASync('index.html', html)
+// }).then(function() {
+//     console.log("Successfully wrote to index.html");
+//   })
+// .catch(function (err){
+//     console.log(err)
+// });
+
+async function init() {
+    console.log("hi")
+    try {
+        const answers = await promptUser();
+
+        const res = await getGit(answers);
+
+        const html = generateHTML(answers, res.data);
+
+        await writeFileAsync("README.md", html);
+
+        console.log("Successfully wrote to README.md");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
